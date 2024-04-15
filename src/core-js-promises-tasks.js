f6342ddb5ef83d687039e1a9bed325d2e901fbd7
ValeriyL01 <lastovka.valeriy.00@mail.ru>
2024-04-15 11:28:23 +0300

    feat: add getAllOrNothing , getAllResult

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -94,8 +94,11 @@ function getFirstPromiseResult(/* promises */) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)] => Promise rejected with 2
  */
-function getAllOrNothing(/* promises */) {
-  throw new Error('Not implemented');
+function getAllOrNothing(promises) {
+  const answer = Promise.all(promises)
+    .then((arrValues) => arrValues)
+    .catch((err) => Promise.reject(err));
+  return answer;
 }
 
 /**
@@ -110,8 +113,10 @@ function getAllOrNothing(/* promises */) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with [1, null, 3]
  */
-function getAllResult(/* promises */) {
-  throw new Error('Not implemented');
+function getAllResult(promises) {
+  const answer = Promise.all(promises.map((value) => value.catch(() => null)));
+
+  return answer;
 }
 
 /**
